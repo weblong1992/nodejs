b631e2e8fb2f94ad244807cecfab5e7d68ac0d06
AllenVue <1173402105@qq.com>
2023-06-23 11:33:04 +0800

    测试

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use((err, req, res, next) => {
   res.cc(err)
 })
 
-console.log('测试1111')
+console.log('测试2222')
 // 启动服务器
 app.listen(3007, () => {
   console.log('api server running at http://127.0.0.1:3007')
